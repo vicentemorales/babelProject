83423a82487260281ab23e69682d114b524f6c66
vicentemorales <vmorales121@gmail.com>
2023-05-16 19:16:06 -0700

    added babel

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//multiplying function
+const multiply = (a,b) => a * b
+const firstNum = 2
+const secondNum = 3
+let product = multiply(firstNum, secondNum)
+console.log(`The product of ${firstNum} and ${secondNum} is ${product}`)
+
